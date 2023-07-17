982e5df822a25831e80c055dd2a24ab0572e37f6
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-07-17 14:32:29 -0300

    first commit

---
+++ src/main/java/br/com/emendes/jornadamilhasapi/JornadaMilhasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emendes.jornadamilhasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaMilhasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaMilhasApiApplication.class, args);
+	}
+
+}
