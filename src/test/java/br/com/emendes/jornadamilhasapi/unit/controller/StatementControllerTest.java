ba3601ddbecfec9d076476732fd3392a2de66652
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-07-19 18:30:25 -0300

    test: testes para o endpoint GET /api/statements

--- src/test/java/br/com/emendes/jornadamilhasapi/unit/controller/StatementControllerTest.java
+++ src/test/java/br/com/emendes/jornadamilhasapi/unit/controller/StatementControllerTest.java
@@ -3,7 +3,9 @@ package br.com.emendes.jornadamilhasapi.unit.controller;
 import br.com.emendes.jornadamilhasapi.controller.StatementController;
 import br.com.emendes.jornadamilhasapi.service.StatementService;
 import br.com.emendes.jornadamilhasapi.service.dto.response.StatementResponse;
+import br.com.emendes.jornadamilhasapi.util.PageableResponse;
 import br.com.emendes.jornadamilhasapi.util.faker.StatementFaker;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,10 +15,17 @@ import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ProblemDetail;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -103,6 +112,32 @@ class StatementControllerTest {
       Assertions.assertThat(actualFields).isNotNull().contains("username");
       Assertions.assertThat(actualMessages).isNotNull()
           .contains("username must not be blank", "username must contain between 2 and 100 characters long");
+    }
+
+  }
+
+  @Nested
+  @DisplayName("Tests for fetch endpoint")
+  class FetchEndpoint {
+
+    private final String urlTemplate = "/api/statements";
+    private final Pageable PAGEABLE_DEFAULT = PageRequest.of(0, 10);
+
+    @Test
+    @DisplayName("fetch must return status 200 and Page<StatementResponse> when fetch successfully")
+    void fetch_MustReturnStatus200AndPageStatementResponse_WhenFetchSuccessfully() throws Exception {
+      BDDMockito.when(statementServiceMock.fetch(PAGEABLE_DEFAULT))
+          .thenReturn(new PageImpl<>(List.of(StatementFaker.statementResponse()), PAGEABLE_DEFAULT, 1));
+
+      String actualContent = mockMvc.perform(get(urlTemplate))
+          .andExpect(status().isOk())
+          .andReturn().getResponse().getContentAsString();
+
+      Page<StatementResponse> actualResponseBody = mapper
+          .readValue(actualContent, new TypeReference<PageableResponse<StatementResponse>>() {
+          });
+
+      Assertions.assertThat(actualResponseBody).isNotNull().isNotEmpty().hasSize(1);
     }
 
   }
